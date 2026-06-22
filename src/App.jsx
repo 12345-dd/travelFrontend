bb9bf9dbe8412cf2c5aca5f8947588841a8e8e74
12345-dd <mayurpatil98607@gmail.com>
2026-06-22 08:29:57 +0530

    Setup application routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import CreateTrip from './pages/CreateTrip'
+import TripDetails from './pages/TripDetails'
 
 
 function App() {
 
   return (
-    <>
-      <h1>Mayur</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/create-trip" element={<CreateTrip />} />
+      <Route path="/trip/:id" element={<TripDetails />} />
+    </Routes>
   )
 }
 
